177369d957a272b962e02c0352e7b75daf12aa87
Nik <kosyankov01@gmail.com>
2024-12-12 22:41:03 +0300

    Initial Commit

---
+++ MobileProjectTests/MobileProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MobileProjectTests.swift
+//  MobileProjectTests
+//
+//  Created by Никита Косянков on 12.12.2024.
+//
+
+import Testing
+@testable import MobileProject
+
+struct MobileProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
